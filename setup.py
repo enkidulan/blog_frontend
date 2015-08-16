3dbacb42f5f5636a3566d4500de5f0c14bef3c27
wercker builder <enkidulan@gmail.com>
2015-08-16 16:43:34 +0000

    Prepare version 1.0.201508161643 [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='blog_frontend',
-      version='v1.0.0',
+      version='1.0.201508161643',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
